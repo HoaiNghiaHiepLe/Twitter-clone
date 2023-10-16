3533bbc51d7ce3ebead61f988e5ff78ff2e59d5f
HoaiNghiaHiepLe <hiep.lhn@gmail.com>
2023-10-17 00:53:00 +0700

    connect to mongo atlas using vscode extension

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import userRoutes from './routes/user.routes'
+import DatabaseService from './services/database.service'
 
 const app = express()
 const port = 3000
@@ -8,6 +9,8 @@ app.use(express.json())
 app.post('/', (req, res) => {
   res.send('Hello World!')
 })
+
+DatabaseService.connect()
 
 app.use('/user', userRoutes)
 
