832f8804bcaf795faa8bb154fffd0388719065a6
HoaiNghiaHiepLe <hiep.lhn@gmail.com>
2023-10-24 00:49:41 +0700

    create user schema | userService | userController | register route

--- src/controllers/users.controllers.ts
+++ src/controllers/users.controllers.ts
@@ -1,4 +1,7 @@
 import { Request, Response } from 'express'
+import User from '~/models/schemas/User.schema'
+import databaseService from '~/services/database.services'
+import userService from '~/services/user.services'
 
 export const loginController = (req: Request, res: Response) => {
   const { email, password } = req.body
@@ -11,4 +14,20 @@ export const loginController = (req: Request, res: Response) => {
   }
 
   res.json({ message: 'Login sucessful' })
+}
+
+export const registerController = async (req: Request, res: Response) => {
+  const { email, password } = req.body
+  try {
+    const result = await userService.register({ email, password })
+    return res.json({
+      message: 'register successful',
+      result
+    })
+  } catch (error) {
+    console.log(error)
+    return res.status(400).json({
+      error: 'Register failed'
+    })
+  }
 }
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express'
-import { loginController } from '~/controllers/users.controllers'
+import { loginController, registerController } from '~/controllers/users.controllers'
 import { loginValidator } from '~/middlewares/users.middlewares'
 
 const usersRoutes = Router()
 
 usersRoutes.post('/login', loginValidator, loginController)
+usersRoutes.post('/register', registerController)
 
 export default usersRoutes
