9d04bab71d212000a4b72c24f2768c9da03a465c
HoaiNghiaHiepLe <hiep.lhn@gmail.com>
2023-11-23 17:49:20 +0700

    Declare Create access and Refresh the User's token

--- src/services/user.services.ts
+++ src/services/user.services.ts
@@ -1,13 +1,35 @@
 import User from '~/models/schemas/User.schema'
 import databaseService from './database.services'
 import { RegisterReqBody } from '~/models/schemas/requests/User.request'
+import { hashPassword } from '~/utils/crypto'
+import { signToken } from '~/utils/jwt'
+import { TokenType } from '~/constant/enum'
 
 class UserService {
+  private signAccessToken(user_id: string) {
+    return signToken({
+      payload: {
+        user_id,
+        tokenType: TokenType.AccessToken
+      }
+    })
+  }
+
+  private signRefreshToken(user_id: string) {
+    return signToken({
+      payload: {
+        user_id,
+        tokenType: TokenType.RefreshToken
+      }
+    })
+  }
+
   async register(payload: RegisterReqBody) {
     const result = await databaseService.users.insertOne(
       new User({
         ...payload,
-        date_of_birth: new Date(payload.date_of_birth as string)
+        date_of_birth: new Date(payload.date_of_birth as string),
+        password: hashPassword(payload.password)
       })
     )
     return result
