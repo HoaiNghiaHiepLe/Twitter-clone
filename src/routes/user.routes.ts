bf2f53ac1db942291cbe8f9f75eaa3983bab0176
HoaiNghiaHiepLe <hiep.lhn@gmail.com>
2023-11-23 10:59:12 +0700

    Validate Requests

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,10 +1,18 @@
 import { Router } from 'express'
 import { loginController, registerController } from '~/controllers/users.controllers'
-import { loginValidator } from '~/middlewares/users.middlewares'
+import { loginValidator, registerValidator } from '~/middlewares/users.middlewares'
 
 const usersRoutes = Router()
 
 usersRoutes.post('/login', loginValidator, loginController)
-usersRoutes.post('/register', registerController)
+
+/**
+ * Description: Register a new user
+ * Path: /register
+ * Method: POST
+ * Body: { name: string, email: string, password: string, confirm_password: string, date_of_birth: ISO8601 }
+ */
+
+usersRoutes.post('/register', registerValidator, registerController)
 
 export default usersRoutes
