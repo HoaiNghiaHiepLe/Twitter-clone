8a10a45f826ae9b34b1d4966b20fad527c56f721
HoaiNghiaHiepLe <hiep.lhn@gmail.com>
2023-12-05 15:47:27 +0700

    logout | access token middleware

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,5 +1,7 @@
 import jwt, { SignOptions } from 'jsonwebtoken'
 import { config } from 'dotenv'
+import { ErrorWithStatus } from '~/models/Errors'
+import HTTP_STATUS from '~/constant/httpStatus'
 
 config()
 
@@ -16,6 +18,23 @@ export const signToken = ({
     jwt.sign(payload, privateKey, options, (error, token) => {
       if (error) reject(error)
       resolve(token as string)
+    })
+  })
+}
+
+export const verifyToken = ({
+  token,
+  secretOrPublicKey = process.env.JWT_SECRET as string,
+  options = { algorithm: 'HS256' }
+}: {
+  token: string
+  secretOrPublicKey?: string
+  options?: SignOptions
+}) => {
+  return new Promise<jwt.JwtPayload>((resolve, reject) => {
+    jwt.verify(token, secretOrPublicKey, options, (error, decoded) => {
+      if (error) throw reject(new ErrorWithStatus({ message: error.message, status: HTTP_STATUS.UNAUTHORIZED }))
+      resolve(decoded as jwt.JwtPayload)
     })
   })
 }
