aeb9a9b43b47c400a5dab144690bd00b9c8222ed
HoaiNghiaHiepLe <hiep.lhn@gmail.com>
2024-01-05 16:24:22 +0700

    Filter Upload Image

--- server/src/controllers/medias.controllers.ts
+++ server/src/controllers/medias.controllers.ts
@@ -1,24 +1,10 @@
 import { Request, Response } from 'express'
-import formidable from 'formidable'
-import path from 'path'
-import { PATH } from '~/constant/path'
+import { handleUploadImage } from '~/utils/file'
 
 export const uploadImageController = async (req: Request, res: Response) => {
-  //? Nếu lỗi import thì dùng cách này
-  // const formidable = await (await import('formidable')).default
-  const form = formidable({
-    uploadDir: path.resolve(PATH.FOLDER.UPLOAD_IMAGE),
-    maxFiles: 1,
-    keepExtensions: true,
-    maxFieldsSize: 300 * 1024 // 300KB,
-  })
-
-  const result = await form.parse(req, (err, fields, files) => {
-    if (err) {
-      throw err
-    }
-    return res.json({
-      message: 'Upload image successfully'
-    })
+  const data = await handleUploadImage(req, res)
+  return res.json({
+    message: 'Upload image successfully',
+    data: data?.file
   })
 }
