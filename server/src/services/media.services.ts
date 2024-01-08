1639eabee91438479fc44d4f10cd8ef57ead924a
HoaiNghiaHiepLe <hiep.lhn@gmail.com>
2024-01-08 14:18:45 +0700

    Check Enviroment from passed arguments

--- server/src/services/media.services.ts
+++ server/src/services/media.services.ts
@@ -4,15 +4,22 @@ import sharp from 'sharp'
 import { DIR } from '~/constant/dir'
 import { getNameFromFullName, handleUploadSingleImage } from '~/utils/file'
 import fs from 'fs'
+import { isProduction } from '~/constant/config'
+import { config } from 'dotenv'
+
+config()
 class MediaService {
   async handleUploadImage(req: Request) {
     const file = await handleUploadSingleImage(req)
     const newName = getNameFromFullName(file.newFilename)
     const newPath = path.resolve(DIR.UPLOAD_IMAGE_DIR, `${newName}.jpg`)
     await sharp(file.filepath).jpeg().toFile(newPath)
-    fs.unlinkSync(file.filepath)
     // delete file in temp folder after upload
-    return `http://localhost:3000/${newName}.jpg`
+    fs.unlinkSync(file.filepath)
+    console.log(isProduction)
+    return isProduction
+      ? `${process.env.HOST}/medias/${newName}.jpg`
+      : `http://localhost:${process.env.PORT}/medias/${newName}.jpg`
   }
 }
 
