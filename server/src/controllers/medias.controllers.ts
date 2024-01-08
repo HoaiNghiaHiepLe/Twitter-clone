cc9dd3632085dad45cd06d7a4371edd91df4c552
HoaiNghiaHiepLe <hiep.lhn@gmail.com>
2024-01-08 14:34:39 +0700

    Serving Static File By Express

--- server/src/controllers/medias.controllers.ts
+++ server/src/controllers/medias.controllers.ts
@@ -2,9 +2,9 @@ import { Request, Response } from 'express'
 import mediaServices from '~/services/media.services'
 
 export const uploadImageController = async (req: Request, res: Response) => {
-  const result = await mediaServices.handleUploadImage(req)
+  const url = await mediaServices.handleUploadImage(req)
   return res.json({
     message: 'Upload image successfully',
-    result
+    url
   })
 }
