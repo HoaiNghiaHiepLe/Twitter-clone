97ae51228f87029bf3fba2bb560386a36d14bf2f
HoaiNghiaHiepLe <hiep.lhn@gmail.com>
2024-01-22 16:23:37 +0700

    Create tweet service

--- server/src/controllers/tweets.controllers.ts
+++ server/src/controllers/tweets.controllers.ts
@@ -2,11 +2,17 @@ import { NextFunction, Request, Response } from 'express'
 import { ParamsDictionary } from 'express-serve-static-core'
 import { config } from 'dotenv'
 import { TweetRequestBody } from '~/models/requests/Tweet.request'
+import tweetServices from '~/services/tweet.services'
+import { TokenPayload } from '~/models/requests/User.request'
 
 config()
 
 export const createTweetController = async (req: Request<ParamsDictionary, any, TweetRequestBody>, res: Response) => {
+  const { user_id } = req.decoded_authorization as TokenPayload
+
+  const result = await tweetServices.createTweet(user_id, req.body)
   return res.json({
-    message: 'Create tweet successfully'
+    message: 'Create tweet successfully',
+    result
   })
 }
--- server/src/models/schemas/Tweet.schema.ts
+++ server/src/models/schemas/Tweet.schema.ts
@@ -5,18 +5,18 @@ import { Media } from '~/types/Media.type'
 //? Những thứ class cần xử lý trước khi đưa vào Database
 interface TweetConstructor {
   _id?: ObjectId
-  user_id: ObjectId
+  user_id: string
   type: TweetType
   audience: TweetAudience
   content: string
-  parent_id: null | ObjectId //  chỉ null khi tweet gốc
+  parent_id: null | string //  chỉ null khi tweet gốc
   hashtags: ObjectId[]
-  mentions: ObjectId[]
+  mentions: string[]
   medias: Media[]
-  guest_views: number
-  user_views: number
-  created_at: Date
-  updated_at: Date
+  guest_views?: number
+  user_views?: number
+  created_at?: Date
+  updated_at?: Date
 }
 
 export default class Tweet {
@@ -30,10 +30,10 @@ export default class Tweet {
   hashtags: ObjectId[]
   mentions: ObjectId[]
   medias: Media[]
-  guest_views: number
-  user_views: number
-  created_at: Date
-  updated_at: Date
+  guest_views?: number
+  user_views?: number
+  created_at?: Date
+  updated_at?: Date
   //? Những thứ class cần xử lý trước khi đưa vào Database
   constructor({
     _id,
@@ -51,17 +51,17 @@ export default class Tweet {
     updated_at
   }: TweetConstructor) {
     this._id = _id
-    this.user_id = user_id
+    this.user_id = new ObjectId(user_id)
     this.type = type
     this.audience = audience
     this.content = content
-    this.parent_id = parent_id
+    this.parent_id = parent_id ? new ObjectId(parent_id) : null
     this.hashtags = hashtags
-    this.mentions = mentions
+    this.mentions = mentions.map((mention: string) => new ObjectId(mention))
     this.medias = medias
-    this.guest_views = guest_views
-    this.user_views = user_views
-    this.created_at = created_at
-    this.updated_at = updated_at
+    this.guest_views = guest_views || 0
+    this.user_views = user_views || 0
+    this.created_at = created_at || new Date()
+    this.updated_at = updated_at || new Date()
   }
 }
