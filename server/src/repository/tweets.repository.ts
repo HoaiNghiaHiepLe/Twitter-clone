7f5f7f5fd753f1188b909937a5af0f08316224dc
HoaiNghiaHiepLe <hiep.lhn@gmail.com>
2024-01-25 12:00:19 +0700

    Get and pagination tweet childrens by skip and limit

--- server/src/repository/tweets.repository.ts
+++ server/src/repository/tweets.repository.ts
@@ -1,4 +1,5 @@
 import { Document, IntegerType, ObjectId, OnlyFieldsOfType } from 'mongodb'
+import { TweetType } from '~/constant/enum'
 import Tweet from '~/models/schemas/Tweet.schema'
 import databaseService from '~/services/database.services'
 
@@ -17,35 +18,10 @@ export const findTweetById = async (tweet_id: string) => {
   return result
 }
 
-export const findAndUpdateTweetById = async (
-  tweet_id: string,
-  inc: OnlyFieldsOfType<Tweet, IntegerType>,
-  projection?: Document
-) => {
-  const result = await databaseService.tweets.findOneAndUpdate(
-    {
-      _id: new ObjectId(tweet_id)
-    },
-    //$inc là operator của mongodb, dùng để tăng giá trị của field truyền vào khi gọi function
-    {
-      $inc: inc,
-      $currentDate: {
-        updated_at: true
-      }
-    },
-    {
-      returnDocument: 'after',
-      projection: projection
-    }
-  )
-
-  return result
-}
-
 const tweetDetailAggregate = (tweet_id: string): Document[] => [
   {
     $match: {
-      _id: new ObjectId('65b0c0f63b146d66a58c4e1f')
+      _id: new ObjectId(tweet_id)
     }
   },
   {
@@ -111,7 +87,7 @@ const tweetDetailAggregate = (tweet_id: string): Document[] => [
       from: 'tweets',
       localField: '_id',
       foreignField: 'parent_id',
-      as: 'tweet_childrent'
+      as: 'tweet_children'
     }
   },
   {
@@ -125,10 +101,10 @@ const tweetDetailAggregate = (tweet_id: string): Document[] => [
       retweets: {
         $size: {
           $filter: {
-            input: '$tweet_childrent',
+            input: '$tweet_children',
             as: 'item',
             cond: {
-              $eq: ['$$item.type', 1]
+              $eq: ['$$item.type', TweetType.Retweet]
             }
           }
         }
@@ -136,10 +112,10 @@ const tweetDetailAggregate = (tweet_id: string): Document[] => [
       comments: {
         $size: {
           $filter: {
-            input: '$tweet_childrent',
+            input: '$tweet_children',
             as: 'item',
             cond: {
-              $eq: ['$$item.type', 2]
+              $eq: ['$$item.type', TweetType.Comment]
             }
           }
         }
@@ -147,10 +123,10 @@ const tweetDetailAggregate = (tweet_id: string): Document[] => [
       quotes: {
         $size: {
           $filter: {
-            input: '$tweet_childrent',
+            input: '$tweet_children',
             as: 'item',
             cond: {
-              $eq: ['$$item.type', 3]
+              $eq: ['$$item.type', TweetType.QuoteTweet]
             }
           }
         }
@@ -159,7 +135,198 @@ const tweetDetailAggregate = (tweet_id: string): Document[] => [
   },
   {
     $project: {
-      tweet_childrent: 0
+      tweet_children: 0
     }
+  }
+]
+
+export const findAndUpdateTweetById = async (
+  tweet_id: string,
+  inc: OnlyFieldsOfType<Tweet, IntegerType>,
+  projection?: Document
+) => {
+  const result = await databaseService.tweets.findOneAndUpdate(
+    {
+      _id: new ObjectId(tweet_id)
+    },
+    //$inc là operator của mongodb, dùng để tăng giá trị của field truyền vào khi gọi function
+    {
+      $inc: inc,
+      $currentDate: {
+        updated_at: true
+      }
+    },
+    {
+      returnDocument: 'after',
+      projection: projection
+    }
+  )
+
+  return result
+}
+
+export const findTweetChildrenByParentId = async ({
+  parent_id,
+  tweet_type,
+  page,
+  limit
+}: {
+  parent_id: string
+  tweet_type: TweetType
+  page: number
+  limit: number
+}) => {
+  const tweets = await databaseService.tweets
+    // Truyền Tweet vào generic type của aggregate để trả về Tweet[]
+    .aggregate<Tweet>(tweetChildrenAggregate({ parent_id, tweet_type, page, limit }))
+    // chuyển AggregationCursor<Document> thành Document[]
+    .toArray()
+
+  const total = await databaseService.tweets.countDocuments({
+    parent_id: new ObjectId(parent_id),
+    type: tweet_type
+  })
+
+  return { tweets, total }
+}
+
+const tweetChildrenAggregate = ({
+  parent_id,
+  tweet_type,
+  page,
+  limit
+}: {
+  parent_id: string
+  tweet_type: TweetType
+  page: number
+  limit: number
+}): Document[] => [
+  {
+    $match: {
+      parent_id: new ObjectId(parent_id),
+      type: tweet_type
+    }
+  },
+  {
+    $lookup: {
+      from: 'hashtags',
+      localField: 'hashtags',
+      foreignField: '_id',
+      as: 'hashtags'
+    }
+  },
+  {
+    $lookup: {
+      from: 'users',
+      localField: 'mentions',
+      foreignField: '_id',
+      as: 'mentions'
+    }
+  },
+  {
+    $addFields: {
+      mentions: {
+        $map: {
+          input: '$mentions',
+          as: 'mention',
+          in: {
+            _id: '$$mention._id',
+            name: '$$mention.name',
+            username: '$$mention.name',
+            email: '$$mention.email'
+          }
+        }
+      },
+      hashtags: {
+        $map: {
+          input: '$hashtags',
+          as: 'hashtag',
+          in: {
+            _id: '$$hashtag._id',
+            name: '$$hashtag.name'
+          }
+        }
+      }
+    }
+  },
+  {
+    $lookup: {
+      from: 'bookmarks',
+      localField: '_id',
+      foreignField: 'tweet_id',
+      as: 'bookmarks'
+    }
+  },
+  {
+    $lookup: {
+      from: 'likes',
+      localField: '_id',
+      foreignField: 'tweet_id',
+      as: 'likes'
+    }
+  },
+  {
+    $lookup: {
+      from: 'tweets',
+      localField: '_id',
+      foreignField: 'parent_id',
+      as: 'tweet_children'
+    }
+  },
+  {
+    $addFields: {
+      likes: {
+        $size: '$likes'
+      },
+      bookmarks: {
+        $size: '$bookmarks'
+      },
+      retweets: {
+        $size: {
+          $filter: {
+            input: '$tweet_children',
+            as: 'item',
+            cond: {
+              $eq: ['$$item.type', TweetType.Retweet]
+            }
+          }
+        }
+      },
+      comments: {
+        $size: {
+          $filter: {
+            input: '$tweet_children',
+            as: 'item',
+            cond: {
+              $eq: ['$$item.type', TweetType.Comment]
+            }
+          }
+        }
+      },
+      quotes: {
+        $size: {
+          $filter: {
+            input: '$tweet_children',
+            as: 'item',
+            cond: {
+              $eq: ['$$item.type', TweetType.QuoteTweet]
+            }
+          }
+        }
+      }
+    }
+  },
+  {
+    $project: {
+      tweet_children: 0
+    }
+  },
+  {
+    // Trang hiện tại
+    $skip: limit * (page - 1) // Công thưc phân trang
+  },
+  {
+    // Số lượng bản ghi mỗi trang
+    $limit: limit
   }
 ]
