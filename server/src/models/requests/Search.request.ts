66317a4d3947cd9141e3d3e0b1643db6bd3badc2
HoaiNghiaHiepLe <hiep.lhn@gmail.com>
2024-01-29 17:57:29 +0700

    Filter tweets by followed people

--- server/src/models/requests/Search.request.ts
+++ server/src/models/requests/Search.request.ts
@@ -1,7 +1,10 @@
 import { MediaRequestQuery } from '~/constant/enum'
 import { Pagination } from './Tweet.request'
+import { Query } from 'express-serve-static-core'
 
 export type SearchRequestQuery = {
   q?: string
   f?: MediaRequestQuery
-} & Pagination
+  pf?: string
+} & Pagination &
+  Query
