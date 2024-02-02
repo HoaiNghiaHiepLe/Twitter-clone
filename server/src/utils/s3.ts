41a70041b97740772f6498cc4466e66a24f8a445
HoaiNghiaHiepLe <hiep.lhn@gmail.com>
2024-02-02 17:26:58 +0700

    Simple upload file to aws s3 | Fix s3 block access on Amazon S3 > Buckets > Permissions

--- server/src/utils/s3.ts
+++ server/src/utils/s3.ts
@@ -1,5 +1,8 @@
+import { Upload } from '@aws-sdk/lib-storage'
 import { S3 } from '@aws-sdk/client-s3'
 import { config } from 'dotenv'
+import fs from 'fs'
+import path from 'path'
 
 config()
 
@@ -12,3 +15,32 @@ const s3 = new S3({
 })
 
 s3.listBuckets({}).then((data) => console.log(data.Buckets))
+const file = fs.readFileSync(path.resolve('uploads/images/c70e3af5949bcee1c8e3f6900.jpg'))
+
+const parallelUploads3 = new Upload({
+  client: s3,
+  params: {
+    // Tên bucket s3 trên aws
+    Bucket: 'twitter-clone-2024',
+    // Tên file sẽ lưu trên s3
+    Key: 'image1.jpg',
+    // Nội dung file sẽ gửi lên s3
+    Body: file,
+    ContentType: 'image/jpeg'
+  },
+
+  tags: [
+    /*...*/
+  ], // optional tags
+  queueSize: 4, // optional concurrency configuration
+  partSize: 1024 * 1024 * 5, // optional size of each part, in bytes, at least 5MB
+  leavePartsOnError: false // optional manually handle dropped parts
+})
+
+parallelUploads3.on('httpUploadProgress', (progress) => {
+  console.log(progress)
+})
+
+parallelUploads3.done().then((data) => {
+  console.log(data)
+})
