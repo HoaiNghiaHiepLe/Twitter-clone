78bf473ae06c9d4f66352948e535c0587b19850b
HoaiNghiaHiepLe <hiep.lhn@gmail.com>
2024-02-03 15:33:57 +0700

    Upload images to s3

--- server/src/index.ts
+++ server/src/index.ts
@@ -14,7 +14,8 @@ import databaseService from './services/database.service'
 import tweetsRouter from './routes/tweets.routes'
 import bookmarksRouter from './routes/bookmarks.routes'
 import searchRouter from './routes/search.routes'
-import '~/utils/s3'
+// test upload file to s3
+// import '~/utils/s3'
 // fake data
 // import '~/utils/faker'
 
--- server/src/utils/s3.ts
+++ server/src/utils/s3.ts
@@ -2,7 +2,6 @@ import { Upload } from '@aws-sdk/lib-storage'
 import { S3 } from '@aws-sdk/client-s3'
 import { config } from 'dotenv'
 import fs from 'fs'
-import path from 'path'
 
 config()
 
@@ -14,33 +13,46 @@ const s3 = new S3({
   }
 })
 
-s3.listBuckets({}).then((data) => console.log(data.Buckets))
-const file = fs.readFileSync(path.resolve('uploads/images/c70e3af5949bcee1c8e3f6900.jpg'))
+// test list bucket
+// s3.listBuckets({}).then((data) => console.log(data.Buckets))
+// test file upload to s3
+// const file = fs.readFileSync(path.resolve('uploads/images/c70e3af5949bcee1c8e3f6900.jpg'))
 
-const parallelUploads3 = new Upload({
-  client: s3,
-  params: {
-    // Tên bucket s3 trên aws
-    Bucket: 'twitter-clone-2024',
-    // Tên file sẽ lưu trên s3
-    Key: 'image1.jpg',
-    // Nội dung file sẽ gửi lên s3
-    Body: file,
-    ContentType: 'image/jpeg'
-  },
+export const uploadFileToS3 = async ({
+  fileName,
+  filePath,
+  ContentType
+}: {
+  fileName: string
+  filePath: string
+  ContentType: string
+}) => {
+  const parallelUploads3 = new Upload({
+    client: s3,
+    params: {
+      // Tên bucket s3 trên aws
+      Bucket: 'twitter-clone-2024',
+      // Tên file sẽ lưu trên s3
+      Key: fileName,
+      // Nội dung file sẽ gửi lên s3
+      Body: fs.readFileSync(filePath),
+      ContentType: ContentType
+    },
 
-  tags: [
-    /*...*/
-  ], // optional tags
-  queueSize: 4, // optional concurrency configuration
-  partSize: 1024 * 1024 * 5, // optional size of each part, in bytes, at least 5MB
-  leavePartsOnError: false // optional manually handle dropped parts
-})
+    tags: [
+      /*...*/
+    ], // optional tags
+    queueSize: 4, // optional concurrency configuration
+    partSize: 1024 * 1024 * 5, // optional size of each part, in bytes, at least 5MB
+    leavePartsOnError: false // optional manually handle dropped parts
+  })
+  return parallelUploads3.done()
+}
 
-parallelUploads3.on('httpUploadProgress', (progress) => {
-  console.log(progress)
-})
+// parallelUploads3.on('httpUploadProgress', (progress) => {
+//   console.log(progress)
+// })
 
-parallelUploads3.done().then((data) => {
-  console.log(data)
-})
+// parallelUploads3.done().then((data) => {
+//   console.log(data)
+// })
