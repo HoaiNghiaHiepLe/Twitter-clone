23bc506c1786a5edac9255cf6292f6fbb8f8b878
HoaiNghiaHiepLe <hiep.lhn@gmail.com>
2024-02-14 17:53:14 +0700

    Get conversations middlewares

--- server/src/models/requests/Common.request.ts
+++ server/src/models/requests/Common.request.ts
@@ -1,16 +1,6 @@
-import { Query } from 'express-serve-static-core'
-import { Pagination } from './Tweet.request'
-
 export interface BookmarkTweetRequestBody {
   tweet_id: string
 }
 export interface LikeTweetRequestBody {
   tweet_id: string
 }
-
-export type ConversationRequestQueryParam = {
-  receiver_id?: string
-  limit?: string
-  page?: string
-} & Pagination &
-  Query
